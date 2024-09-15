6ca3744c08cdc605d99afb3246b0b1525d5e24e8
George Ibarra <gibarr13@stu.southtexascollege.edu>
2024-09-15 17:34:39 -0500

    First commit of all the files

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+import {ref} from 'vue'
+
+const name = ref('')
+
+</script>
+
+<template>
+
+ <main class="app">
+
+<section class="greetin">
+<h2 class="title">
+  Welcome back, <input type="placeholder=Enter Name" v-model="name">
+</h2>
+</section>
+
+<section class="create-Countdown">
+
+</section>
+
+
+<section class="countdown-list">
+
+</section>
+
+
+ </main>
+
+</template>
